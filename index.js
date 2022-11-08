291abd2920eb67fd5afa0593b0a05d400cc1fb14
mehrabriyan <mehraabriyan@gmail.com>
2022-11-08 21:59:57 +0600

    working on leftsidebar

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const leftsidebar = document.querySelector(".left-sidebar");
-const btngroup = document.querySelector(".btn-group");
-const leftbutton = document.querySelector("#left");
+const leftSidebar = document.querySelector(".left-sidebar");
+const btnGroup = document.querySelector(".btn-group");
+const leftButton = document.querySelector("#left");
+const leftColse = document.querySelector("#left-close");
 
-leftbutton.addEventListener("click", () => {
-    console.log("click");
-    leftsidebar.style.visibility = "visible";
+leftButton.addEventListener("click", () => {
+    leftSidebar.style.animation = "leftsidebar 1s linear 0.5s both";
+    leftSidebar.style.opacity = 1;
+    leftSidebar.style.visibility = "visible";
+});
+
+leftColse.addEventListener("click", () => {
+    leftSidebar.style.visibility = "hidden";
+    leftSidebar.style.opacity = 0;
+    leftSidebar.style.animation = "none";
 });
